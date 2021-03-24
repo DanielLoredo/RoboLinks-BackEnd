4a81586db1a317a118531fe3f8fbca9bd475b395
teapot2 <A01197460@itesm.mx>
2021-03-24 17:46:31 -0600

    Added filtering given parameters

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -26,4 +26,25 @@ class Link extends Model
     {
         return $this->hasOne(Tag::class); //link_id en tags
     }
+
+    public function scopeFilter($query)
+    {
+        if (request('title')) {
+            $query->where('title', request('title'));
+        }
+
+        if (request('private')) {
+            $query->where('private', request('private'));
+        }
+
+        if (request('short_url')) {
+            $query->where('short_url', request('short_url'));
+        }
+
+        if (request('url')) {
+            $query->where('url', request('url'));
+        }
+
+        return $query;
+    }
 }
